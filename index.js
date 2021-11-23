c326a32b41c663d3ec05a5d72b24accb97c639a3
Sarah Nosal <nosalsm@gmail.com>
2021-11-23 08:50:47 -0600

    commit tree db5c682a554cc0094c1cc8273c9eeffd6ebd8a40

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback) {
+   callback();
+}
+function returnsANamedFunction() {
+    return function hello() {};
+}
+function returnsAnAnonymousFunction() {
+    return function () {};
+}
